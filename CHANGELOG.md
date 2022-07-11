657d2ff4d0ef4be4e231ed2e90776e071ac5d088
azhinu <ari@ariellab.tk>
2022-07-11 15:00:41 +0300

    Updated deps, added syntax `source.` prefix compatibility.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to the "grammar-mode" extension will be documented in this file.
 
+## [0.0.3]
+
+- Now supported grammar names starting with `source.`
+- Updated deps
+
 ## [0.0.2]
 
 - Removed forgotten debug messages.
--- src/extension.ts
+++ src/extension.ts
@@ -35,10 +35,15 @@ export async function applySyntax(doc: any) {
   let syntax = doc.getText(new Range(doc.lineAt(0).range.start, doc.lineAt(textLines).range.end))
   // Regexp syntax=$
   syntax = syntax.match(/(?<=syntax=)(?:\w+[-.]?)+/g)
+
   // If not null
   if (syntax) {
     // Use only first element
     syntax = syntax[0]
+
+    // If syntax have `source.` prefix
+    if (syntax.startsWith('source.')) syntax = syntax.replace('source.', '')
+
     // Check languages id is exists
     if (!(await languages.getLanguages()).includes(syntax)) {
       window.showWarningMessage('Wrong syntax id',syntax)
